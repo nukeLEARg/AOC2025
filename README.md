244e7bb7015a2e73b6c9fd2e310693d0f643649b
Nuke <nukelearyt@gmail.com>
2025-11-28 13:27:36 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# AOC2025 ocaml
+
+Advent of Code 2025 solutions in OCaml.
+
+## Description
+
+Advent of Code 2025 solutions implemented in OCaml using Dune build system. For later conversion to Hardcaml as part of Jane Street Advent of FPGA.
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
---
+++ bin/Runner.ml
@@ -0,0 +1,39 @@
+open Core
+
+let time_execution f =
+  let start_time = Time_ns.now () in
+  let result = f () in
+  let end_time = Time_ns.now () in
+  let elapsed = Time_ns.diff end_time start_time in
+  printf "Execution time: %s" (Time_ns.Span.to_string elapsed);
+  result
+;;
+
+let time_execution_t f =
+  let start_time = Time_ns.now () in
+  let result = f () in
+  let end_time = Time_ns.now () in
+  let elapsed = Time_ns.diff end_time start_time in
+  printf "\nTotal Execution time: %s\n" (Time_ns.Span.to_string elapsed);
+  result
+;;
+
+let dayrunner (d : int) =
+  for i = 1 to d do
+    let _ = printf "\nExecuting Day %02d%!" i in
+    match
+      time_execution (fun () ->
+        Core_unix.system (sprintf "./_build/default/bin/Day%02d.exe" i))
+    with
+    | Ok () -> printf "\n"
+    | err -> printf "\nCommand failed: %s\n" (Core_unix.Exit_or_signal.to_string_hum err)
+  done
+;;
+
+let () =
+  printf "Enter the number of days to execute: %!";
+  let input = In_channel.input_line_exn In_channel.stdin in
+  let num_days = Int.of_string input in
+  let _ = printf "Starting Execution of %d Days\n%!" num_days in
+  time_execution_t (fun () -> dayrunner num_days)
+;;
